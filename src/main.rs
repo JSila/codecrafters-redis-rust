372e774683587c16340b9e39341518cd5a6fc2ad
Jernej Sila <jernej.sila@gmail.com>
2023-02-02 18:14:39 +0100

    respond to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    let mut buf = [0; 512];
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buf = [0; 512];
-                stream.read(&mut buf).unwrap();
+            Ok(mut stream) => loop {
+                let bytes_read = stream.read(&mut buf).unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
                 stream.write_all(b"+PONG\r\n").unwrap();
-            }
+            },
             Err(e) => {
                 eprintln!("error: {}", e);
             }
